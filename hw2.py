72a1e7106c8f500c0ca0cac33ad2ab68f7ee1d90
KACHOK24 <asdbek625500@gmail.com>
2026-09-04 15:15:35 +0500

    yangilandi

--- hw2.py
+++ hw2.py
@@ -1,17 +1,26 @@
-# juft = []
-# toq = []
+# katta = 0
+
+# for i in range(5):
+#     son = int(input("Son kiriting: "))
+#     if son > katta:
+#         katta = son
+# print("Eng katta son:", katta)
+
+
+
+# juft = 0
+# toq = 0
 
 # for i in range(10):
 #     son = int(input("Son kiriting: "))
 
 #     if son % 2 == 0:
-#         juft.append(son)
+#         juft+=1
 #     else:
-#         toq.append(son)
+#         toq +=1
 
 # print("Juft sonlar:", juft)
 # print("Toq sonlar:", toq)
-
 
 # parol = ("py1234")
 
@@ -21,8 +30,8 @@
 #    print("xush kelipsiz")
 #   else:
 #    print("parol notg'ri")
-son = int(input("Son kiriting: "))
+# son = int(input("Son kiriting: "))
 
-for i in range(1, son + 1):
-    if son % i == 0:
-        print(i)
+# for i in range(1, son + 1):
+#     if son % i == 0:
+#         print(i)
